e1326376a09c6434d9f720831d98611d70bf924b
deepbas <deepbas99@gmail.com>
2025-03-17 11:05:57 -0400

    3-17-1

---
+++ setup.R
@@ -0,0 +1,16 @@
+# setup.R
+# Load required libraries
+library(dplyr)
+library(ggplot2)
+library(fpp3)
+library(patchwork)
+library(tsibbledata)
+library(urca)
+library(astsa)
+library(gridExtra)
+library(vars)
+library(fable)
+library(feasts)
+library(tidyquant)
+library(quantmod)
+library(purrr)
